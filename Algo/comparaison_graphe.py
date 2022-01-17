c6bb94bc7d9dac39188158c8eb944b4c9b04d8c5
Gwendal Troadec <gw8ndal@gmail.com>
2022-01-17 11:58:02 +0100

    fin du cours du 17/01/22

--- Algo/comparaison_graphe.py
+++ Algo/comparaison_graphe.py
@@ -1,13 +1,18 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from comparaison import *
+from tri import *
 
 def comparer_graphe(f1, f2):
     result = comparer(f1, f2)
     plt.figure(figsize=(10, 10))
-    x = [r[0] for r in results]
-    y1 = [r[1] for r in results]
-    y2 = [r[1] for r in results]
+    x = [r[0] for r in result]
+    y1 = [r[1] for r in result]
+    y2 = [r[2] for r in result]
     plt.scatter(x, y1, label='Function 1')
     plt.scatter(x, y2, label='Function 2')
     plt.xlabel('Elements in list')
     plt.ylabel("Time (ms)")
+    plt.legend()
+    plt.grid(True)
+    return plt.show()
+    
