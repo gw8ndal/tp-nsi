d338572a9c095cb72e9d3dc33d8807abebb10bb4
gw8ndal <61947142+gw8ndal@users.noreply.github.com>
2022-01-17 10:35:42 +0100

    Delete pascal_bourrin.py

--- pascal_bourrin.py
+++
@@ -1,8 +0,0 @@
-def pascal_bourrin(n, p):
-    if p == 0 or n == p:
-        return 1
-    else:
-        p1 = pascal_bourrin(n-1, p-1)
-        p2 = pascal_bourrin(n-1, p)
-        return p1 + p2
-print(pascal_bourrin(50,30))
