5c8577f7d55f6afa45d85f75771c4852f407d472
Gwendal Troadec <gw8ndal@gmail.com>
2022-01-21 09:22:43 +0100

    ajout du sujet 17

--- exercices-tp/sujet-17/ex1.py
+++ exercices-tp/sujet-17/ex1.py
@@ -2,8 +2,19 @@ def indice_du_min(tab):
     """
     Renvoie l'indice du minimum de la liste donnée
     """
-    
-    
-indice_du_min([5])
-indice_du_min([2, 4, 1])
-indice_du_min([5, 3, 2, 2, 4])
+    assert type(tab) == list
+    tmp = [i for i in tab]
+    while len(tmp) > 1:
+        if tmp[0] > tmp[1]:
+            tmp.pop(0)
+        else:
+            mini = tmp[0]
+            tmp.pop(1)
+    if len(tmp) == 1:
+        mini = tmp[0]
+    return tab.index(mini)
+
+
+print(indice_du_min([5]))
+print(indice_du_min([2, 4, 1]))
+print(indice_du_min([5, 3, 2, 2, 4]))
