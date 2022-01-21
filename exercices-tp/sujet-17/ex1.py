98745bc4efc7ec61219f04bd3e63a6c4e77dc336
Gwendal Troadec <gw8ndal@gmail.com>
2022-01-21 09:22:32 +0100

    ajout du sujet 17

---
+++ exercices-tp/sujet-17/ex1.py
@@ -0,0 +1,9 @@
+def indice_du_min(tab):
+    """
+    Renvoie l'indice du minimum de la liste donnée
+    """
+    
+    
+indice_du_min([5])
+indice_du_min([2, 4, 1])
+indice_du_min([5, 3, 2, 2, 4])
