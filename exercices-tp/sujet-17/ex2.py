dfc0808112f64aa356e188f88bc7c32e1787f5dc
Gwendal Troadec <gw8ndal@gmail.com>
2022-01-21 09:34:07 +0100

    sujet 17 ex2 fait

--- exercices-tp/sujet-17/ex2.py
+++ exercices-tp/sujet-17/ex2.py
@@ -1,10 +1,13 @@
 def separe(tab):
     i = 0
-    j = ...
+    j = len(tab) -1
     while i < j :
         if tab[i] == 0 :
-            i = ...
+            i += 1
         else :
-            tab[i], tab[j] = ...
-            j = ...
+            tab[i], tab[j] = tab[j], tab[i]
+            j -= 1
     return tab
+
+print(separe([1, 0, 1, 0, 1, 0, 1, 0]))
+print(separe([1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0]))
