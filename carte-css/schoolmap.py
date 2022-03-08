20c72f2c607a67f3771437ce5722f42e7290ea25
Gwendal Troadec <gw8ndal@gmail.com>
2022-03-08 17:20:21 +0100

    carte des collèges terminées, informations à modifier

--- carte-css/schoolmap.py
+++ carte-css/schoolmap.py
@@ -1,5 +1,6 @@
 import requests as r
 import csv
+import folium as f
 
 def get_csv_data(url, filename):
     """
@@ -17,11 +18,23 @@ def get_csv_data(url, filename):
 
 def read_data(file):
     with open(file) as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.DictReader(csvfile, delimiter=';')
         for row in reader:
-            print(row)
+            lat, longitude = row['point_geo_ban'].split(',')
+            lat = float(lat)
+            longitude = float(longitude)
+            build_school(row, map, lat, longitude)
+    return map
+            
 
+def build_school(row, map, lat, longitude):
+    html_code = f"{row['nom_et']}"
+    icon_color = "red"
+    return f.Marker([lat, longitude],popup = html_code, icon = f.Icon(color = icon_color)).add_to(map)
 
+map = f.Map(location=[48.518042, -3.943186])
 fichier = get_csv_data('https://www.data.gouv.fr/fr/datasets/r/a1075259-09ca-4c22-9e35-b2cd6bbf5c36', 'schools.csv')
-print(fichier)
-read_data(fichier)
+
+school_data = read_data(fichier)
+
+school_data.save('colleges.html')
