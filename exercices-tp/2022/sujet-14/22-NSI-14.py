97943a2638a9afb1b1a9db571b0345bdbc769623
Gwendal Troadec <gw8ndal@gmail.com>
2022-03-18 09:50:08 +0100

    tp 14 a finir

---
+++ exercices-tp/2022/sujet-14/22-NSI-14.py
@@ -0,0 +1,23 @@
+def correspond(mot, mot_a_trous):
+    for i in range(len(mot_a_trous)):
+        if mot[i] == mot_a_trous[i]:
+
+
+
+
+def est_cyclique(plan):
+    '''
+    Prend en paramètre un dictionnaire `plan` correspondant 
+    à un plan d'envoi de messages entre `N` personnes A, B, C, 
+    D, E, F ...(avec N <= 26).
+    Renvoie True si le plan d'envoi de messages est cyclique
+    et False sinon. 
+    '''
+    personne = 'A'
+    N = len(...)                          
+    for i in range(...):
+        if plan[...] == ...:
+            return ...
+        else:
+            personne = ...
+    return ...
