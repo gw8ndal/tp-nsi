2b784eb8b3c66d1370121e0000ae6c1617ad6011
Gwendal Troadec <gw8ndal@gmail.com>
2022-03-18 21:49:38 +0100

    sujet 14 2022 ex1 terminé

--- exercices-tp/2022/sujet-14/22-NSI-14.py
+++ exercices-tp/2022/sujet-14/22-NSI-14.py
@@ -1,23 +1,34 @@
 def correspond(mot, mot_a_trous):
-    for i in range(len(mot_a_trous)):
-        if mot[i] == mot_a_trous[i]:
+    assert type(mot) == str
+    assert type(mot_a_trous) == str
 
+    if len(mot) != len(mot_a_trous):
+        return False
+    for i in range(len(mot)):
+        if mot[i] != mot_a_trous[i] and mot_a_trous[i] != '*':
+            return False
+    return True
 
-
+# print(correspond('INFORMATIQUE', 'INFO*MA*IQUE'))
 
 def est_cyclique(plan):
     '''
-    Prend en paramètre un dictionnaire `plan` correspondant 
-    à un plan d'envoi de messages entre `N` personnes A, B, C, 
+    Prend en paramètre un dictionnaire plan correspondant
+    à un plan d'envoi de messages entre N personnes A, B, C,
     D, E, F ...(avec N <= 26).
     Renvoie True si le plan d'envoi de messages est cyclique
-    et False sinon. 
+    et False sinon.
     '''
     personne = 'A'
-    N = len(...)                          
-    for i in range(...):
-        if plan[...] == ...:
-            return ...
+    N = len(plan)
+    for i in range(N-1):
+        if plan[personne] == personne:
+            return False
         else:
-            personne = ...
-    return ...
+            personne = plan[personne][0]
+    return True
+
+print(est_cyclique({'A':'E', 'F':'A', 'C':'D', 'E':'B', 'B':'F', 'D':'C'}))
+print(est_cyclique({'A':'E', 'F':'C', 'C':'D', 'E':'B', 'B':'F', 'D':'A'}))
+print(est_cyclique({'A':'B', 'F':'C', 'C':'D', 'E':'A', 'B':'F', 'D':'E'}))
+print(est_cyclique({'A':'B', 'F':'A', 'C':'D', 'E':'C', 'B':'F', 'D':'E'}))
