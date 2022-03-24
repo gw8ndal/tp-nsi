e725f2abde7818c19d7905727529fcb6c8e11e4c
Gwendal Troadec <gw8ndal@gmail.com>
2022-03-24 18:33:58 +0100

    sujet 2 2022 ex1 fini

--- exercices-tp/2022/sujet-2/22-NSI-02.py
+++ exercices-tp/2022/sujet-2/22-NSI-02.py
@@ -3,7 +3,11 @@ def moyenne(liste):
     renvoie la moyenne pondérée de la liste
     liste : couple de notes et de coefficients
     '''
-    pass    
+    return sum([i[0] * i[1] for i in liste])/sum([j[1] for j in liste])
+    # return sum([i[0] * i[1] for i in liste])/sum([j[0] for j in liste])
+
+print(moyenne([(15,2),(9,1),(12,3)]))
+
 
 def pascal(n):
     C= [[1]]
