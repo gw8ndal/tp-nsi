a630cf31aa8f9a2bc31caf8f874f99d95618d2cd
Gwendal Troadec <gw8ndal@gmail.com>
2022-04-03 14:32:56 +0200

    sujet 37 fini

--- exercices-tp/2022/sujet-37/22-NSI-37.py
+++ exercices-tp/2022/sujet-37/22-NSI-37.py
@@ -1,22 +1,40 @@
+#Exercice 1
+def verifie(tab):
+    '''
+    Renvoie True si le tableau en entrée est trié dans l'ordre croissant, False sinon
+    '''
+    assert type(tab) == list
+    
+    if sorted(tab) == tab:
+        return True
+    return False
+
+print(verifie([0, 5, 8, 8, 9]))
+print(verifie([8, 12, 4]))
+print(verifie([-1, 4]))
+print(verifie([5]))
+
+# Exercice 2
 urne = ['A', 'A', 'A','B', 'C', 'B', 'C','B', 'C', 'B']
 
 def depouille(urne):
-    resultat = ...
+    resultat = {}
     for bulletin in urne:
-        if ...:
+        if bulletin in resultat:
             resultat[bulletin] = resultat[bulletin] + 1
         else:
-            ...
+            resultat[bulletin] = 1
     return resultat
 
 def vainqueur(election):
     vainqueur = ''
     nmax = 0
     for candidat in election:
-        if ... > ... :
-            nmax = ...
+        if election[candidat] > nmax :
+            nmax = election[candidat]
             vainqueur = candidat
-    liste_finale = [nom for nom in election if election[nom] == ...]
-    return ...
+    liste_finale = [nom for nom in election if election[nom] == nmax]
+    return liste_finale
 
-
+election = depouille(urne)
+print(vainqueur(election))
